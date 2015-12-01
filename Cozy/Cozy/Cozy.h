ae25de7360fcaff99fa573fa35c03db78e663a62
Evan JIANG <firstfan@gmail.com>
2015-12-01 12:27:42 +0900

    Create empty cozy framework and add to CozyApp

---
+++ Cozy/Cozy/Cozy.h
@@ -0,0 +1,19 @@
+//
+//  Cozy.h
+//  Cozy
+//
+//  Created by Evan JIANG on 15/12/1.
+//  Copyright © 2015年 Kii Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Cozy.
+FOUNDATION_EXPORT double CozyVersionNumber;
+
+//! Project version string for Cozy.
+FOUNDATION_EXPORT const unsigned char CozyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cozy/PublicHeader.h>
+
+
---
+++ Cozy/Cozy/Cozy.swift
@@ -0,0 +1,19 @@
+//
+//  Cozy.swift
+//  Cozy
+//
+//  Created by Evan JIANG on 15/12/1.
+//  Copyright © 2015年 Kii Inc. All rights reserved.
+//
+
+import Foundation
+import CoreBluetooth
+
+public class Cozy : NSObject {
+    
+    public override init() {
+        
+    }
+    
+    
+}
